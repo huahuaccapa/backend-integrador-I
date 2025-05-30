a963d7629a5c53128b0a46f62814e115adde8a2e
huahuaccapa <huahuaccapa1119@gmail.com>
2025-05-30 08:44:01 -0500

    fix: corrección en las consultas del ProductoRepository

--- src/main/java/com/nico/multiservicios/repository/ProductoRepository.java
+++ src/main/java/com/nico/multiservicios/repository/ProductoRepository.java
@@ -1,7 +1,17 @@
 package com.nico.multiservicios.repository;
 
 import com.nico.multiservicios.model.Producto;
+import com.nico.multiservicios.model.TipoProducto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
+    List<Producto> findByStockGreaterThan(int stock);
+    List<Producto> findByNombreProductoContaining(String nombre);
+    List<Producto> findByNombreProductoContainingIgnoreCase(String nombre); // Añade este método
+    List<Producto> findByEstado(String estado);
+    List<Producto> findByTipoProducto(TipoProducto tipoProducto);
+}   
