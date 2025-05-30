2b81c2b0b92a5f7d62d5440d26cf36da1eac15a2
huahuaccapa <huahuaccapa1119@gmail.com>
2025-05-30 08:44:23 -0500

    feat: repositorio para DetalleVenta implementado

---
+++ src/main/java/com/nico/multiservicios/repository/DetalleVentaRepository.java
@@ -0,0 +1,12 @@
+package com.nico.multiservicios.repository;
+
+import com.nico.multiservicios.model.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+    List<DetalleVenta> findByVentaId(Long ventaId);
+}
